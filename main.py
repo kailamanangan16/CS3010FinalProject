d41a34db7d77f676e0f5f4b947d4d8f3e4cc7bc1
kailamanangan16 <74679729+kailamanangan16@users.noreply.github.com>
2025-05-06 10:25:50 -0700

    implementation changes

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 # Equations (first variable is the result)
-finalEquations = [
-    ["x'''''", "x''''","y","z","t'","p''''"],
-    ["y'''''", "y", "x''"], 
-    ["z'''''", "z", "y''''"],
-    ["t'''''", "z''''", "z''", "z", "y"],
-    ["p'''''", "x", "y", "z", "t", "p"]
-]
-
 startEquations = [
     ["x'''''", "x","y","z","t","p"],
     ["y'''''", "y", "x"], 
@@ -15,17 +7,36 @@ startEquations = [
     ["p'''''", "x", "y", "z", "t", "p"]
 ]
 
-def sumVar():
-    pass
+answerEquations = [
+    ["x'''''", "x''''","y","z","t'","p''''"],
+    ["y'''''", "y", "x''"], 
+    ["z'''''", "z", "y''''"],
+    ["t'''''", "z''''", "z''", "z", "y"],
+    ["p'''''", "x", "y", "z", "t", "p"]
+]
 
 def integrateVar(var):
     return var + "'"
 
-def equateVar(integratedList, answerList):
-    pass
+
+# -> start = startEquations[i] 
+# -> answer = answerEquations[i]
+def sumVar(start, answer): # makes all the start equation variables integrate ready
+    for i in range(1,len(start)): # i = the variable that needs to be integrated
+        while start[i] != answer[i]:
+            start[i] = integrateVar(start[i])
+            
+# -> start = startEquations[i] 
+# -> answer = answerEquations[i]
+def equateVar(start, answer): #if these two are equal return true
+    return start == answer
 
 def run():
+    result = True
     for i in range(len(startEquations)):
-        pass
+        sumVar(startEquations[i],answerEquations[i])
+        if not equateVar(startEquations[i], answerEquations[i]):
+            result = False
+    return result
 
-run()
+print(run())
